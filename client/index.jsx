aab49ba0a0a11a774cb76ba60769fb0ca8c2b5bc
Marco Biedermann <marco.biedermann@gmx.de>
2017-11-12 14:28:49 +0100

    move App into its own component

--- client/index.jsx
+++ client/index.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const App = () => (
-  <h1>Hello, world</h1>
-);
+import App from './components/App';
 
 render(<App />, document.getElementById('root'));
