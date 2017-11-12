4efbd0a7c7d3e98b7580ec2a8076dd1e55f10934
Marco Biedermann <marco.biedermann@gmx.de>
2017-11-12 14:19:51 +0100

    add webpack and webpack-dev-sever

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  devServer: {
+    contentBase: path.resolve(__dirname, 'docs'),
+    hot: true,
+  },
+  devtool: 'source-map',
+  entry: {
+    main: [
+      path.resolve(__dirname, 'client/index'),
+    ],
+  },
+  output: {
+    filename: 'assets/js/[name].js',
+    path: path.resolve(__dirname, 'docs'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+    ],
+  },
+  plugins: isDevelopment ? [
+    new webpack.HotModuleReplacementPlugin(),
+  ] : [
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      output: {
+        comments: false,
+      },
+      sourceMap: true,
+    }),
+  ],
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx',
+    ],
+  },
+};
