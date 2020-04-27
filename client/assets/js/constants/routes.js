799592aaad05705f8cf8fddb3892dbbdb45daf81
Marco Biedermann <marco.biedermann@gmx.de>
2020-04-27 13:32:47 +0200

    refactor: routes
    Refactor routes object

--- client/assets/js/constants/routes.js
+++ client/assets/js/constants/routes.js
@@ -1,22 +1,18 @@
 import { lazy } from 'react';
 
-const IndexPage = lazy(() => import('../pages/IndexPage'));
-const PageA = lazy(() => import('../pages/PageA'));
-const PageB = lazy(() => import('../pages/PageB'));
-
 const routes = {
   INDEX: {
-    component: IndexPage,
+    component: lazy(() => import('../pages/IndexPage')),
     name: 'Home',
     path: '/',
   },
   PAGE_A: {
-    component: PageA,
+    component: lazy(() => import('../pages/PageA')),
     name: 'Page A',
     path: '/page-a',
   },
   PAGE_B: {
-    component: PageB,
+    component: lazy(() => import('../pages/PageB')),
     name: 'Page B',
     path: '/page-b',
   },
