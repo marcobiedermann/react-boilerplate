61a5a099e48c1ddd6c264ea013731b83c982fb43
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-04-27 11:24:24 +0200

    feat: Sentry
    Add error tracking using Sentry

--- src/assets/js/main.tsx
+++ src/assets/js/main.tsx
@@ -1,6 +1,14 @@
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Root from './components/Root';
 import './i18n';
+
+Sentry.init({
+  dsn: 'https://96e995fc8aec42be8a7a5ef7c3b51afb@o81678.ingest.sentry.io/6365383',
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 createRoot(document.getElementById('root')!).render(<Root />);
