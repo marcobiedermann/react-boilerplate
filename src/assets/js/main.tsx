61fd2d268630ef80cf16a1d83e2c5cd4321ccc03
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2022-04-28 18:45:03 +0200

    fix: Sentry Environment
    Set Sentry environment

--- src/assets/js/main.tsx
+++ src/assets/js/main.tsx
@@ -4,9 +4,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Root from './components/Root';
 import './i18n';
+import { environment } from './utilities/environment';
 
 Sentry.init({
   dsn: 'https://96e995fc8aec42be8a7a5ef7c3b51afb@o81678.ingest.sentry.io/6365383',
+  environment,
   integrations: [new BrowserTracing()],
   tracesSampleRate: 1.0,
 });
