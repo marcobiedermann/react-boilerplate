db224e6264617f121eada6c8c23f272ac571c7df
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com.>
2024-04-23 16:42:26 +0200

    fix: Fixes
    General fixes and updates

--- src/assets/js/@types/i18next.d.ts
+++ src/assets/js/@types/i18next.d.ts
@@ -1,7 +1,7 @@
-import 'react-i18next';
+import 'i18next';
 import translation from '../../../../public/locales/en/translation.json';
 
-declare module 'react-i18next' {
+declare module 'i18next' {
   interface CustomTypeOptions {
     resources: {
       translation: typeof translation;
